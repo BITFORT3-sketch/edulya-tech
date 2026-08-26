e019020e6d8f52d5c4b7047eab3d9dfe2b50c1b3
BITFORT3-sketch <bitdoudivin@gmail.com>
2026-08-27 00:59:48 +0100

    Update config.js

--- js/config.js
+++ js/config.js
@@ -4,4 +4,4 @@
 // Une fois le backend déployé sur Render, remplace cette valeur par ton URL publique,
 // ex: 'https://edulya-tech-api.onrender.com'
 
-const API_URL = 'http://127.0.0.1:5000';
+const API_URL = 'https://edulya-tech.onrender.com';
